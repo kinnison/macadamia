3c468d30f96614a716a7637fcf78aa94c3f47358
Daniel Silverstone <dsilvers@digital-scurf.org>
2025-01-02 17:32:22 +0000

    proto1-code: Bring CPU up to 48MHz

--- proto1-code/src/main.rs
+++ proto1-code/src/main.rs
@@ -3,11 +3,26 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
+use embassy_stm32::{
+    rcc::{self, Sysclk},
+    Config,
+};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
+    let mut config = Config::default();
+    config.rcc.hsi = true;
+    config.rcc.hse = None;
+    config.rcc.pll = Some(rcc::Pll {
+        src: rcc::PllSource::HSI,
+        mul: rcc::PllMul::MUL12,
+        prediv: rcc::PllPreDiv::DIV2,
+    });
+    config.rcc.sys = Sysclk::PLL1_P;
+    let _p = embassy_stm32::init(config);
+
     loop {
         Timer::after_secs(1).await;
         info!("Hello World");
