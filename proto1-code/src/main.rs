8a04425cb27be43ceba03bbdf4b485c460c2e4d6
Daniel Silverstone <dsilvers@digital-scurf.org>
2025-01-02 18:55:11 +0000

    proto1-code: Trivial latency measurer

--- proto1-code/src/main.rs
+++ proto1-code/src/main.rs
@@ -7,11 +7,11 @@ use embassy_stm32::{
     rcc::{self, Sysclk},
     Config,
 };
-use embassy_time::Timer;
+use embassy_time::{Duration, Instant, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) -> ! {
+async fn main(spawner: Spawner) -> ! {
     let mut config = Config::default();
     config.rcc.hsi = true;
     config.rcc.hse = None;
@@ -23,8 +23,31 @@ async fn main(_spawner: Spawner) -> ! {
     config.rcc.sys = Sysclk::PLL1_P;
     let _p = embassy_stm32::init(config);
 
+    spawner.spawn(latency_measure()).unwrap();
+
     loop {
-        Timer::after_secs(1).await;
-        info!("Hello World");
+        Timer::after_secs(5).await;
+        info!("Five Seconds");
+    }
+}
+
+#[embassy_executor::task]
+async fn latency_measure() {
+    loop {
+        let start = Instant::now();
+        let mut next = start + Duration::from_millis(1);
+        let mut total_gap = Duration::from_secs(0);
+        for _ in 0..10_000 {
+            Timer::at(next).await;
+            let gap = Instant::now().saturating_duration_since(next);
+            total_gap += gap;
+            next += Duration::from_millis(1);
+        }
+        let elapsed = Instant::now().saturating_duration_since(start);
+        info!(
+            "Elapsed: {}µs, total gap: {}µs",
+            elapsed.as_micros(),
+            total_gap.as_micros()
+        );
     }
 }
